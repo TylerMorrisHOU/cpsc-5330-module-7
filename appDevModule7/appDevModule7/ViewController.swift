027144f7d04ee9e85d5e2554d9c977d55a32c720
Tyler Morris <tyler.david.morris@gmail.com>
2024-07-08 22:30:08 -0500

    Added tableView

--- appDevModule7/appDevModule7/ViewController.swift
+++ appDevModule7/appDevModule7/ViewController.swift
@@ -7,7 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Row " + String(indexPath.row)
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
+    var cells = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
